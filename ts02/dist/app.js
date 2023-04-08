cce6cccb54468f74ff7d301e46a1135ca68f667d
ogzhnozcn13 <o.ozcan.91@gmail.com>
2023-04-08 19:30:52 +0300

    generics

--- ts02/dist/app.js
+++ ts02/dist/app.js
@@ -1,2 +1,127 @@
 "use strict";
 console.log("ozi");
+//? classes --------------------------------------------
+class Employee {
+    constructor(code, name) {
+        this.empCode = code;
+        this.empName = name;
+    }
+}
+let emp1 = new Employee(1, 'Mark');
+// let emp2 = new Employee('1', 'Mark')
+//? Inheritance ---------------------------------------
+class Person {
+    constructor(name) {
+        this.name = name;
+    }
+}
+class Employee2 extends Person {
+    constructor(emplyCode, name) {
+        super(name);
+        this.emplyCode = emplyCode;
+    }
+    display() {
+        console.log(` Name: ${this.name}  - code ${this.emplyCode}`);
+    }
+}
+const empl1 = new Employee2(2113, 'Yasin');
+const per1 = new Person('Feyza');
+//? Abstract class -------------------------------------
+class Vehicle {
+    constructor(year, color) {
+        this.year = year;
+        this.color = color;
+    }
+    startEngine() {
+        console.log(`Engine Started`);
+    }
+}
+class Car extends Vehicle {
+    constructor(year, color, model, numOfDoors) {
+        super(year, color);
+        this.model = model;
+        this.numOfDoors = numOfDoors;
+    }
+    stopEngine() {
+        console.log(' Car stopped !');
+    }
+}
+const car = new Car(1984, 'blue', 'bmw', 4);
+//? Data modifiers - Private ------------------------
+// hiçbir şekilde dışarıdan ve child'dan erişilemez
+class Person2 {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+    getAge() {
+        console.log(this.age);
+    }
+    setAge(newAge) {
+        if (newAge > 0 && newAge <= 100) {
+            this.age = newAge;
+        }
+        else {
+            throw new Error('Invalid Age');
+        }
+    }
+}
+const Barry = new Person2('Barry', 25);
+Barry.getAge();
+Barry.setAge(26);
+Barry.getAge();
+//? protected -----------------------------------------
+// privet'ın bir alt gizlilik durumu
+// child erişir ama dışarıdan erişilemez
+//? read-only -----------------------------------------
+// ilk değeri değiştirilemez
+//? static -------------------------------------------
+class Circle {
+    static calcArea(radius) {
+        return this.pi * radius * radius;
+    }
+}
+Circle.pi = 3.14;
+console.log(Circle.pi);
+console.log(Circle.calcArea(5));
+class LMSCalender {
+    constructor(events) {
+        this.events = events;
+    }
+    addEvents(event) {
+        this.events.push(event);
+    }
+}
+const cohort13 = new LMSCalender(['HTML', 'CSS', 'JS']);
+console.log(cohort13);
+cohort13.addEvents('Typescript');
+console.log(cohort13);
+const red = { color: { r: 255, g: 0, b: 0 } };
+let shape1 = { area: 255 };
+class Square {
+    constructor(color, area) {
+        this.color = color;
+        this.area = area;
+    }
+}
+const sq = new Square({ r: 0, g: 0, b: 100 }, 255);
+console.log(sq);
+function add(n1, n2) {
+    return n1 + n2;
+}
+function conC(n1, n2) {
+    return n1 + n2;
+}
+let arithemticAdd = add;
+console.log(arithemticAdd(2, 5));
+// console.log(arithemticAdd(2,"ozi"));
+// let conCt:Sum = conC
+//? generics ----------------------------------------------
+//? Problem & Solution ------------------------------------
+function getArray(items) {
+    return new Array().concat(items);
+}
+let numArr = getArray([1, 2, 3, 4]);
+let strArr = getArray(['john', 'Mike', 'Alan']);
+// strArr.push(3)  /* Error */
+// numArr.push('Sally') /* Error */
--- ts02/src/app.ts
+++ ts02/src/app.ts
@@ -1,1 +1,215 @@
 console.log("ozi");
+
+//? classes --------------------------------------------
+
+class Employee {
+    empCode: number;
+    empName: string;
+
+    constructor (code:number, name:string){
+        this.empCode = code;
+        this.empName = name
+    }
+}
+let emp1 = new Employee(1, 'Mark')
+// let emp2 = new Employee('1', 'Mark')
+
+
+
+
+//? Inheritance ---------------------------------------
+
+class Person{
+    name: string;
+    constructor(name: string){
+        this.name = name
+    }
+}
+
+class Employee2 extends Person{
+    emplyCode: number;
+
+    constructor(emplyCode: number, name: string){
+        super(name);
+        this.emplyCode = emplyCode;
+    }
+
+    display():void{
+        console.log(` Name: ${this.name}  - code ${this.emplyCode}`)
+    }
+}
+
+const empl1 = new Employee2 (2113, 'Yasin')
+const per1 = new Person('Feyza')
+
+
+
+
+//? Abstract class -------------------------------------
+
+abstract class Vehicle {
+
+    year: number;
+    color: string;
+    constructor(year: number, color: string) {
+      this.year = year;
+      this.color = color;
+    }
+  
+    startEngine(): void {
+      console.log(`Engine Started`)
+    }
+  
+    abstract stopEngine(): void;
+  }
+
+  class Car extends Vehicle {
+  
+    model: string;
+    numOfDoors: number
+    constructor(year: number, color: string, model: string, numOfDoors: number) {
+      super(year, color);
+      this.model = model;
+      this.numOfDoors = numOfDoors
+    }
+  
+    stopEngine(): void {
+        console.log(' Car stopped !')
+    }
+  
+  }
+  const car = new Car(1984, 'blue', 'bmw', 4);
+
+
+
+//? Data modifiers - Private ------------------------
+// hiçbir şekilde dışarıdan ve child'dan erişilemez
+  class Person2 {
+      public name: string;
+      private age: number;
+      constructor(name: string, age: number){
+          this.name = name;
+          this.age = age;
+      }
+  
+      getAge(){
+          console.log(this.age)
+      }
+  
+      setAge(newAge: number){
+          if(newAge > 0 && newAge <=100){
+              this.age = newAge
+          }else{
+              throw new Error('Invalid Age')
+          }
+      }
+  
+  }
+  
+  const Barry = new Person2('Barry',25)
+  
+  Barry.getAge()
+  Barry.setAge(26)
+  Barry.getAge()
+
+
+//? protected -----------------------------------------
+// privet'ın bir alt gizlilik durumu
+// child erişir ama dışarıdan erişilemez
+
+
+
+//? read-only -----------------------------------------
+// ilk değeri değiştirilemez
+
+
+//? static -------------------------------------------
+class Circle {
+    static pi: number = 3.14;
+  
+    static calcArea(radius:number) {
+      return this.pi * radius * radius
+    }
+  }
+  
+console.log(Circle.pi)
+console.log(Circle.calcArea(5))
+
+
+//? interfaces ---------------------------------------
+//ilave'ler yapabiliriz
+interface Calender {
+    events: string[];
+    addEvents(event: string): void;
+}
+
+class LMSCalender implements Calender{
+    constructor(public events: string[]){}
+
+    addEvents(event: string): void {
+        this.events.push(event);
+    }
+}
+
+const cohort13 = new LMSCalender(['HTML','CSS', 'JS'])
+console.log(cohort13)
+
+cohort13.addEvents('Typescript')
+console.log(cohort13)
+
+
+
+//? interface color ---------------------------------------------
+interface Color {
+    color: {r: number, g: number, b: number};
+}
+
+const red:Color = {color: {r: 255, g:0, b:0}}
+
+interface Shape {
+    area: number;
+}
+
+let shape1: Shape = {area:255}
+
+class Square implements Color, Shape {
+    constructor( public color: {r: number, g: number, b:number}, public area: number){
+    }
+}
+
+const sq = new Square({r:0,g:0,b:100},255)
+console.log(sq)
+
+
+
+//? ınterfaces as s function -------------------------------------
+interface Sum{
+    (num1: number, num2: number): number;
+}
+
+function add(n1: number, n2: number): number{
+    return n1+ n2;
+}
+
+function conC(n1: string, n2: string): string{
+    return n1+n2;
+}
+
+let arithemticAdd:Sum = add;
+console.log(arithemticAdd(2,5));
+// console.log(arithemticAdd(2,"ozi"));
+
+// let conCt:Sum = conC
+
+
+//? generics ----------------------------------------------
+//? Problem & Solution ------------------------------------
+
+function getArray<T>(items:T[]): T[]{
+    return new Array().concat(items)
+}
+
+let numArr = getArray([1,2,3,4])
+let strArr = getArray(['john', 'Mike', 'Alan'])
+// strArr.push(3)  /* Error */
+// numArr.push('Sally') /* Error */
